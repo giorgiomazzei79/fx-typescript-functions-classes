0a7f55e432f5f3274e0f3bccf7b579e84bad58c6
Giorgio Mazzei <40761205+giorgiomazzei79@users.noreply.github.com>
2023-05-12 18:24:28 +0200

    feat: add compare function

---
+++ src/helpers/compare.ts
@@ -0,0 +1,9 @@
+const compare = <ObjectToCompare>(obj1: ObjectToCompare, obj2: ObjectToCompare, k: keyof ObjectToCompare): number => {
+    if (obj1[k] < obj2[k]) {
+        return -1
+    }
+    if (obj1[k] > obj2[k]) {
+        return 1
+    }
+    return 0
+}
